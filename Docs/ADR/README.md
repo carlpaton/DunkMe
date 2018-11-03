7ae21f850cca0839189593399cbdf29b51379c27
Carl Paton <cpaton@trademe.local>
2018-11-03 15:55:56 +1300

    Game models and ADR

--- Docs/ADR/README.md
+++ Docs/ADR/README.md
@@ -6,6 +6,7 @@ This log lists the architectural decisions for adr-log.
 - [ADR-0001](0001-front-end.md) - Front End
 - [ADR-0002](0002-data-access-api.md) - Data Access API
 - [ADR-0003](0003-relational-database.md) - Relational Database
+- [ADR-0004](0004-game-database-fields.md) - Game Database Fields
 
 For new ADRs, please use [template.md](https://github.com/charleyza/adr-log/blob/master/docs/adr/template.md) as basis. 
 
---
+++ Repository/Schema/GameModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Repository.Schema
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public int GameTypeId { get; set; }
+        public DateTime InsertDate { get; set; }
+    }
+
+    public class GameLineModel
+    {
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        /// <summary>
+        /// 1 if the ball goes in
+        /// 0 if it misses
+        /// verbose logging like this will make reporting easier and leave nothing to interpretation
+        /// </summary>
+        public int Score { get; set; }
+        public DateTime InsertDate { get; set; }
+    }
+}
